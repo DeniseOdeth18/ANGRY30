77f18fb687bcc955e86cc27215af3ca99bd6e77e
DeniseOdeth18 <89421213+DeniseOdeth18@users.noreply.github.com>
2021-11-08 12:50:55 -0600

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,26 @@
+
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+
+    //console
+   
+    //this
+    
+  }
+
+  display(){
+    console.log(this.body.speed);
+   //condicion
+    if(){
+     
+    }
+    else{
+     
+      //hacer nada
+    }
+    
+  }
+
+}
